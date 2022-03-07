d705e4eaf2430c8a3b337746a478098e0f5f1850
Raphael Araújo <raphox.araujo@gmail.com>
2022-03-06 22:51:54 -0300

    Fixed image path

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import SingleCard from "./components/SingleCard";
 import "./App.scss";
 
 const cardImages: CardImage[] = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "img/helmet-1.png" },
+  { src: "img/potion-1.png" },
+  { src: "img/ring-1.png" },
+  { src: "img/scroll-1.png" },
+  { src: "img/shield-1.png" },
+  { src: "img/sword-1.png" },
 ];
 
 function App() {
